ba6e6745e3a8632f66a974f40dfe35925c9e6a92
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-08-04 20:17:21 +0700

    chore: add script file

--- app-runner.sh
+++ app-runner.sh
@@ -2,9 +2,7 @@
 
 INVENTORY_FILE=".ansible/hosts"
 PLAYBOOK_FILE=".ansible/install-docker.yml"
-EXTRA_ARGS="--syntax-check"
 
 
 echo "Run Playbook..."
-ansible-playbook $EXTRA_ARGS $PLAYBOOK_FILE
 ansible-playbook $PLAYBOOK_FILE -i $INVENTORY_FILE 
