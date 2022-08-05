168c53b0a5193780cd8475d2e32c4f54cdd3817e
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-08-05 16:40:01 +0700

    chore: fixing app-run.sh

--- app-runner.sh
+++ app-runner.sh
@@ -6,3 +6,9 @@ PLAYBOOK_FILE=".ansible/install-docker.yml"
 
 echo "Run Playbook..."
 ansible-playbook $PLAYBOOK_FILE -i $INVENTORY_FILE 
+
+echo "Build Docker-Compose..."
+docker-compose -H "ssh://username@srv1" -f /home/username/stack/.docker/docker-compose.yml up --build -d
+docker-compose -H "ssh://username@srv1" -f /home/username/stack/.docker/docker-compose.yml ps -a
+
+echo "Done..."
