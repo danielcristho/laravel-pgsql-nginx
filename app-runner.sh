64a5992cbf5e720aae20fd8f42790088caf640df
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-08-06 00:28:38 +0700

    feat: update script file

--- app-runner.sh
+++ app-runner.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-INVENTORY_FILE=".ansible/hosts"
-PLAYBOOK_FILE_1=".ansible/install-docker.yml"
-PLAYBOOK_FILE_2=".ansible/deploy-stack.yml"
+INVENTORY_FILE="ansible/hosts"
+PLAYBOOK_FILE_1="ansible/install-docker.yml"
+PLAYBOOK_FILE_2="ansible/deploy-stack.yml"
 
 echo "Run Playbook..."
 ansible-playbook $PLAYBOOK_FILE_1 -i $INVENTORY_FILE 
