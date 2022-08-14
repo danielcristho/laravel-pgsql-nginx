339cde938d9e172b1db675289277766e2e1ea0d5
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-08-14 14:23:04 +0700

    update potgres config

--- docker/pgsql/init.sql
+++ docker/pgsql/init.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE laravel;
-CREATE USER admin WITH ENCRYPTED PASSWORD 'admin123;'
+CREATE USER admin with encrypted password 'admin1234';
 GRANT ALL PRIVILEGES ON DATABASE laravel TO admin;
 \C laravel;
 CREATE EXTENSION IF NOT EXISTS timescaledb;
